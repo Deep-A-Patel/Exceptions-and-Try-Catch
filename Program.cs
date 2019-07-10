ff97841f78f127a87cbe483a6148dbdeb3cf7f42
Deep Patel <deep.ag123007@gmail.com>
2019-07-10 15:56:10 -0500

    done some try and catch

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Calculator calculator = new Calculator();
+                int answer = calculator.Divide(42, 0);
+                Console.WriteLine($"The answer is {answer}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something other than dividing by zero went wrong!");
+            }
+        }
+    }
+}
